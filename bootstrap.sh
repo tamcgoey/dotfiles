7486d3b4a28fed9f555291eee040c2cc8a40f892
Thomas McGoey-Smith <thomas.mcgoeysmith@shopify.com>
2021-11-10 11:09:11 -0800

    add bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+# setup zsh
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+# setup prezto
+ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
+ln -sf ~/dotfiles/.zprofile ~/.zprofile
+
+# config git
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitignore ~/.gitignore
