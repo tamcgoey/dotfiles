e44d171abfacab273902ede76073686ae61b45ed
Thomas McGoey-Smith <thomas.mcgoeysmith@shopify.com>
2021-11-10 11:19:52 -0800

    config

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,15 @@
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 
 # setup prezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+# setup prezto
 ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
 ln -sf ~/dotfiles/.zprofile ~/.zprofile
 
 # config git
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore ~/.gitignore
+
+# chsh
+chsh -s /bin/zsh
